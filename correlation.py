d6ab9e2b49e779ab961430db24856aabf0f40d09
athulbaburaj <athulbaburajp23@gmail.com>
2021-07-01 15:56:58 +0530

    Added the entire code the first time

--- correlation.py
+++ correlation.py
@@ -1,1 +1,67 @@
+
 # Add the functions in this file
+import json
+import math
+
+def load_journal(filename):
+	f=open(filename,"r")
+	data=json.loads(f.read())
+	
+	f.close()
+	return data
+
+def compute_phi(filename,event):
+	n11=n00=n10=n01=n1p=n0p=np1=np0=0
+	journal=load_journal(filename)
+	for i in range(0,len(journal),1):
+		jevents=journal[i].get("events")
+		status=journal[i].get("squirrel")
+		if(event in jevents and status):
+			n11+=1
+			n1p+=1
+			np1+=1
+		else if(event in jevents and (not status)):
+			n10+=1 
+			n1p+=1
+			np0+=1
+		else if((not event in jevents) and status)
+			n01+=1
+			n0p+=1
+			np1+=1
+		else if((not event in jevents) and (not status))
+			n00+=1
+			n0p+=1
+			np0+=1
+	corr=((n11*n00)-(n10*n01))/math.sqrt(n1p*n0p*np1*np0)
+	return corr
+
+def compute_correlations(filename):
+	journal=load_journal(filename)
+	jevents=[]
+	for i in range(0,len(journal),1):
+		for val in journal[i].values():
+			if val in jevents:
+				continue
+			else
+				jevents.append(val)
+	dictcorr={}
+	for eve in jevents:
+		dictcorr[eve]=compute_phi(filename,eve)
+	return dictcorr
+
+def diagnose(filename):
+	cdict=compute_correlations(filename)
+	max_value=max(cdict.values())
+	min_value=min(cdict.values())
+	for key,value in cdict.items():
+		if max_value==value:
+			return key
+	for key,value in cdict.items():
+                if min_value==value:
+                        return key
+
+
+def main():
+	load_journal("journal.json")
+
+main()
